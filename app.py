fbdc467da6b8f5c16f01ce02a4a4f34de9d04af9
Tony Stark <tony-stark-snyk@protonmail.com>
2024-10-26 15:05:58 +0200

    Updating app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import antigravity
 
-SEED=c4947ac9-7814-463a-b390-7d3e19cfef5b
+CERT="""-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+QyNTUxOQAAACB3vHnvW2FIPVMFzkYkrVCbc6ojosxnTKPlW3EiQp2twgAAAJCQiIxAkIiM
+QAAAAAtzc2gtZWQyNTUxOQAAACB3vHnvW2FIPVMFzkYkrVCbc6ojosxnTKPlW3EiQp2twg
+AAAECrbIrsYxANbix19a0mR2arwKxwuB3mXeSUoCwgVixQQne8ee9bYUg9UwXORiStUJtz
+qiOizGdMo+VbcSJCna3CAAAABm15LWtleQECAwQFBgc=
+-----END OPENSSH PRIVATE KEY-----
+"""
 
 def main():
     antigravity.fly()
